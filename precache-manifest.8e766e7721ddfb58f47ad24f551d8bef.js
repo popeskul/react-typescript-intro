b658c15f544cd1e2442e26c4e09b1670a94c482c
Popeskul Pavlo <peterjj91@gmail.com>
2020-04-05 02:08:25 +0300

    Updates

---
+++ precache-manifest.8e766e7721ddfb58f47ad24f551d8bef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798e66b23291941f817d43ae22c331e9",
+    "url": "/react-typescript-intro/index.html"
+  },
+  {
+    "revision": "8a0e45ab265703318f53",
+    "url": "/react-typescript-intro/static/js/2.86674ba6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-typescript-intro/static/js/2.86674ba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4620cdd366d5edcee338",
+    "url": "/react-typescript-intro/static/js/main.0ca0e9dd.chunk.js"
+  },
+  {
+    "revision": "0c90852c2d017311429a",
+    "url": "/react-typescript-intro/static/js/runtime-main.ac51b4bc.js"
+  }
+]);
